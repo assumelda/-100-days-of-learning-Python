e521fe92f9a6136c067a2b0a637ce3bdce26e377
assumelda <assumelda@gmail.com>
2025-11-23 15:32:29 +0100

    My first Commit

---
+++ Band generator.py
@@ -0,0 +1,26 @@
+# #repeating the smae line of code multiple times in differnt lines
+# # print("Hello world!\nHello world!\nHello world!")
+# # print("Hello " + "Angela")
+# #Colleting data from the user
+# # print("Hello "+ input("What is your Name?") + "!")
+# #Variable decleration
+# name="Jack"
+# print(name)
+
+# name="Chisimdi"
+# print(name)
+
+# # # # name=input("What is your name?")
+# # # # print("My name is " +name)
+# # # # print(len(name))
+# # # print(len(input("What is your name?")))
+# Username=input("What is your name?")
+# Length=len(Username)
+# print(Length)
+
+print("Welcome to the Band Name Generator.")
+City_name=input("what is the name of the City you grew up in?\n")
+#print(City_name)
+Pet_name=input("What is your Pet's name?\n")
+#print(Pet_name)
+print("Your band name could be: "+City_name+" "+Pet_name+".")
---
+++ Tip Calculator.py
@@ -0,0 +1,40 @@
+# #String data type, Subscripting
+# print("Hello"[-1])
+# #"123" is a string
+
+# #integer=Whole number
+# print(123+345)
+# #Large integers
+# print(123_456_789)
+# #Float=decimal numbers
+# print(3.147)
+# #Boolean=true or false
+# print(True)
+# print(False)
+
+
+# #print(len("1234"))
+# # print(type("Hello"))
+# # print(type(1234))
+# # print(type(3.147))
+# # print(type(False))
+
+# # #data type conversion
+# # print(int("123")+int("123"))
+
+# # print("Number of letters in your name: "+str(len(input("Enter your name"))))
+
+# #mathematical Operators
+# print(3+4)
+# print(7-5)
+# print(6*4)
+# #/ gives result as float datatype
+# print(4/2)
+# # // gives result as integer data type but doesn't round off
+# print(7//2)
+# # ** for power
+# print(3**2)
+# # Python follows PEMDAS left to Right
+# print(3*3+3/3-3)
+# print(3*(3+3)/3-3)
+# print(3+3-3/3*3)
