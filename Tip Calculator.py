a0d0c381740ce26f33d17d9d7c6db7f4bd39b16c
assumelda <assumelda@gmail.com>
2025-11-24 13:54:17 +0100

    just adding

--- Tip Calculator.py
+++ Tip Calculator.py
@@ -1,48 +1,48 @@
 # #String data type, Subscripting
-# print("Hello"[-1])
-# #"123" is a string
+print("Hello"[-1])
+#"123" is a string
 
-# #integer=Whole number
-# print(123+345)
-# #Large integers
-# print(123_456_789)
-# #Float=decimal numbers
-# print(3.147)
+#integer=Whole number
+print(123+345)
+#Large integers
+print(123_456_789)
+#Float=decimal numbers
+print(3.147)
 # #Boolean=true or false
-# print(True)
-# print(False)
+print(True)
+print(False)
 
 
-# #print(len("1234"))
-# # print(type("Hello"))
-# # print(type(1234))
-# # print(type(3.147))
-# # print(type(False))
+print(len("1234"))
+print(type("Hello"))
+print(type(1234))
+print(type(3.147))
+print(type(False))
 
-# # #data type conversion
-# # print(int("123")+int("123"))
+# #data type conversion
+print(int("123")+int("123"))
 
-# # print("Number of letters in your name: "+str(len(input("Enter your name"))))
+print("Number of letters in your name: "+str(len(input("Enter your name"))))
 
-# #mathematical Operators
-# print(3+4)
-# print(7-5)
-# print(6*4)
-# #/ gives result as float datatype
-# print(4/2)
-# # // gives result as integer data type but doesn't round off
-# print(7//2)
-# # ** for power
-# print(3**2)
-# # Python follows PEMDAS left to Right
-# print(3*3+3/3-3)
-# print(3*(3+3)/3-3)
-# print(3+3-3/3*3)
+#mathematical Operators
+print(3+4)
+print(7-5)
+print(6*4)
+#/ gives result as float datatype
+print(4/2)
+# // gives result as integer data type but doesn't round off
+print(7//2)
+# ** for power
+print(3**2)
+# Python follows PEMDAS left to Right
+print(3*3+3/3-3)
+print(3*(3+3)/3-3)
+print(3+3-3/3*3)
 
 bmi=84/1.65**2
 print(bmi)
 
-# #floors the Value
+#floors the Value
 # print(int(bmi))
 
 #rounding off
